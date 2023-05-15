a82856dad294f42416d46f9ca864a0fcc91e387f
kevin <kevin@C>
2023-05-14 22:06:20 -0500

    Agregar archivos de proyecto.

---
+++ ejemplo unidad 3/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim calculadora As New MiCalculadora()
+
+        calculadora.MostrarMensaje("¡Hola, soy una calculadora!")
+
+        Dim resultadoSuma As Integer = calculadora.Suma(5, 3)
+        MessageBox.Show("Suma: " & resultadoSuma.ToString())
+
+        Dim resultadoResta As Integer = calculadora.Resta(10, 7)
+        MessageBox.Show("Resta: " & resultadoResta.ToString())
+
+        Dim resultadoMultiplicacion As Integer = calculadora.Multiplicacion(4, 6)
+        MessageBox.Show("Multiplicación: " & resultadoMultiplicacion.ToString())
+
+        Try
+            Dim resultadoDivision As Double = calculadora.Division(10, 2)
+            MessageBox.Show("División: " & resultadoDivision.ToString())
+        Catch ex As DivideByZeroException
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
+
---
+++ ejemplo unidad 3/MiCalculadora.vb
@@ -0,0 +1,29 @@
+﻿Public Class MiCalculadora
+    Public Sub MostrarMensaje(mensaje As String)
+        MessageBox.Show(mensaje)
+    End Sub
+
+    Public Function Suma(a As Integer, b As Integer) As Integer
+        Return a + b
+    End Function
+
+    Public Function Resta(a As Integer, b As Integer) As Integer
+        Return a - b
+    End Function
+
+    Public Function Multiplicacion(a As Integer, b As Integer) As Integer
+        Return a * b
+    End Function
+
+    Public Function Division(a As Integer, b As Integer) As Double
+        If b <> 0 Then
+            Return CDbl(a) / CDbl(b)
+        Else
+            Throw New DivideByZeroException("Error: división entre cero no permitida")
+        End If
+    End Function
+End Class
+
+
+
+
